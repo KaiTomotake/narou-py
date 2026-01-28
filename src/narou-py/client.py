3255c5a7c1c519938e7f6353602dc81665d6300b
KaiTomotake <kaitomotake@gmail.com>
2026-01-28 21:57:23 +0900

    add client.py
    Clientクラスを追加

---
+++ src/narou-py/client.py
@@ -0,0 +1,22 @@
+import enum
+import urllib.parse
+
+class ProxyType(enum.Enum):
+    Http = enum.auto()
+    Https = enum.auto()
+
+class Client:
+    BASE = "https://api.syosetu.com"
+
+    def __init__(self, proxies: dict[ProxyType, str]) -> None:
+        self.proxies = dict()
+        for proxy_type, url in proxies.items():
+            result = urllib.parse.urlparse(url)
+            if all([result.scheme, result.netloc]):
+                match proxy_type:
+                    case ProxyType.Http:
+                        self.proxies.update(http=url)
+                    case ProxyType.Https:
+                        self.proxies.update(https=url)
+            else:
+                raise ValueError("Please use a valid proxy URL!")
